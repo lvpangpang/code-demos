7ae2e72b23412c345139bb102982c4aecdc5ec56
lvpangpang <574818075@qq.com>
2023-08-08 17:27:38 +0800

    feat:测试v-if

--- vue-message/src/App.vue
+++ vue-message/src/App.vue
@@ -2,13 +2,24 @@
   <div id="app">
     <button @click="testMessage('success')">测试Message</button>
     <button @click="testMessage('error')">测试Message</button>
+    <button @click="showTest=!showTest">切换test</button>
+    <test :ey="showTest"></test>
   </div>
 </template>
 
 <script>
+import Test from './components/Test'
 let index = 1
 export default {
   name: "App",
+  data() {
+    return {
+      showTest: true
+    }
+  },
+  components: {
+    Test
+  },
   methods: {
     testMessage(type) {
       this.$message1[type](`吕肥肥喜欢上幼儿园${index++}`)
---
+++ vue-message/src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>这是一个测试组件</div>
+</template>
+
+<script>
+console.log(1111111)
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  destroyed() {
+    console.log('destroyed')
+  }
+}
+</script>
